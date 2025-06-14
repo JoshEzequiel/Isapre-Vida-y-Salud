1ac05f029bb722dcd89f895cc0e60a510674a1b8
Gene0409 <gen.lincoqueo@duocuc.cl>
2025-06-13 23:33:44 -0400

    Update Actividad Práctica Pre Evaluación Joshua, Diego, Genesis.py
    Arreglos echos

--- Actividad Práctica Pre Evaluación Joshua, Diego, Genesis.py
+++ Actividad Práctica Pre Evaluación Joshua, Diego, Genesis.py
@@ -1,20 +1,27 @@
+afiliados = [None] * 100
+contador = 0
+montos = [1250, 1300, 1100, 1450, 1380, 1200, 1150, 1275, 1325, 1000]
+indice_monto = 0
+
 while True:
-    print("\n--- MENÚ ISAPRE VIDA Y SALUD ---")
+    print("\n************** MENÚ ISAPRE VIDA Y SALUD ************")
     print("1. Registrar nuevo afiliado")
     print("2. Listar todos los afiliados")
     print("3. Buscar afiliado por RUT")
-    print("4. Modificar datos de un afiliado")
-    print("5. Eliminar afiliado")
-    print("6. Salir")
-    print("7. Imprimir certificado")
+    print("4. Imprimir certificado")
+    print("5. Salir")
+    print("*******************************************************")
+    
 
-    opcion = input("Seleccione una opción (1-7): ")
+    opcion = input("Seleccione una opción (1-5): ")
+    print("---------------------------------------------------------")
 
     if opcion == "1":
         if contador >= 100:
             print(" No se pueden agregar más afiliados. Límite alcanzado.")
         else:
-            print("\n--- Registro de nuevo afiliado ---")
+            print("---------------------------------------------------------")
+            print("\n********* Registro de nuevo afiliado *********")
             rut = input("Ingrese RUT: ")
             nombre = input("Ingrese nombre: ")
             apellido = input("Ingrese apellido paterno: ")
@@ -22,16 +29,19 @@ while True:
             estado_civil = input("Ingrese estado civil: ")
             genero = input("Ingrese género (M/F/Otro): ")
             fecha_afiliacion = input("Ingrese fecha de afiliación (DD-MM-YYYY): ")
+            print("***************************************************")
 
             afiliado = [rut, nombre, apellido, edad, estado_civil, genero, fecha_afiliacion]
             afiliados[contador] = afiliado
             contador = contador + 1
             print(" Afiliado registrado correctamente.")
+            print("*************************************")
 
     elif opcion == "2":
-        print("\n--- Lista de afiliados ---")
+        print("\n********** Lista de afiliados *********")
         if contador == 0:
             print("No hay afiliados registrados.")
+            print("*************************************")
         else:
             i = 0
             while i < contador:
@@ -53,58 +63,6 @@ while True:
             print(" Afiliado no encontrado.")
 
     elif opcion == "4":
-        print("\n****** Modificar afiliado *******")
-        rut_modificar = input("Ingrese el RUT del afiliado a modificar: ")
-        encontrado = False
-        i = 0
-        while i < contador:
-            if afiliados[i][0] == rut_modificar:
-                print("Afiliado encontrado. Ingrese nuevos datos   :")
-                nombre = input("Nuevo nombre                       : ")
-                apellido = input("Nuevo apellido paterno           : ")
-                edad = input("Nueva edad                           : ")
-                estado_civil = input("Nuevo estado civil           : ")
-                genero = input("Nuevo género (M/F/Otro)            : ")
-                fecha_afiliacion = input("Nueva fecha de afiliación: ")
-
-                afiliados[i][1] = nombre
-                afiliados[i][2] = apellido
-                afiliados[i][3] = edad
-                afiliados[i][4] = estado_civil
-                afiliados[i][5] = genero
-                afiliados[i][6] = fecha_afiliacion
-
-                print(" Afiliado modificado correctamente.")
-                encontrado = True
-            i = i + 1
-        if not encontrado:
-            print(" Afiliado no encontrado.")
-
-    elif opcion == "5":
-        print("\n***** Eliminar afiliado ******")
-        rut_eliminar = input("Ingrese el RUT del afiliado a eliminar: ")
-        encontrado = False
-        i = 0
-        while i < contador:
-            if afiliados[i][0] == rut_eliminar:
-                g = i
-                while g < contador - 1:
-                    afiliados[g] = afiliados[g + 1]
-                    g = g + 1
-                afiliados[contador - 1] = None
-                contador = contador - 1
-                print(" Afiliado eliminado correctamente.")
-                encontrado = True
-                break
-            i = i + 1
-        if not encontrado:
-            print(" Afiliado no encontrado.")
-
-    elif opcion == "6":
-        print(" Saliendo del programa. ¡Hasta luego!")
-        break
-
-    elif opcion == "7":
         print("\n******* Imprimir certificado *******")
         rut_certificado = input("Ingrese el RUT del afiliado para el certificado: ")
         i = 0
@@ -135,6 +93,18 @@ while True:
             i = i + 1
         if not encontrado:
             print(" Afiliado no encontrado.")
+            print("*************************************")
+    elif opcion == "5":
+        print("*************************************")
+        print("\n Saliendo del programa.......")
+        print("Desarrollado por:")
+        print("- Joshua Ríos")
+        print("- Genesis Lincoqueo")
+        print("- Diego Almarza")
+        print("Versión del programa: 1.0")
+        print("*************************************")
+        break
 
     else:
         print("Opción no válida. Intente nuevamente.")
+
